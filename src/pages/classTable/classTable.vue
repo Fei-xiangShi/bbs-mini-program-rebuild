6bd4d3b8ef86fac6d2be8b270a9db524f5d5665a
Fei-xiangShi <595834560@qq.com>
2023-08-17 16:24:41 +0800

    课表解析完成

--- src/pages/classTable/classTable.vue
+++ src/pages/classTable/classTable.vue
@@ -2,23 +2,53 @@
   <view>
     <u-button type="primary" @tap="refreshClassTable">刷新</u-button>
   </view>
-  <view class="json">{{ classTable }}</view>
+  <view>
+    <u-scroll-list>
+      <view
+        v-for="item in [
+          1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+        ]"
+        :key="item"
+      >
+        <view @tap="toggleNow" :id="String(item)"> 第{{ item }}周 </view>
+      </view>
+    </u-scroll-list>
+  </view>
+  <view class="classTable" v-for="item in briefList[now]">
+    <view class="class">{{ item }}</view>
+  </view>
+  
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 import Api from "@/public/api";
 import RouteConfig from "@/config/routes";
+import { onLoad } from "@dcloudio/uni-app";
+import formatClassTable from "@/utils/formatClassTable";
 
-let classTable = ref(uni.getStorageSync("classTable"));
+const now = ref(0);
+
+const toggleNow = (e: any) => {
+  now.value = Number(e.target.id);
+  console.log(now.value);
+};
 
 const refreshClassTable = () => {
   Api.getClassTable().then((res: any) => {
     uni.setStorageSync("classTable", res.data || []);
-    classTable.value = res.data || [];
     uni.reLaunch({ url: RouteConfig.classTable.path });
   });
 };
+
+let briefList = ref(uni.getStorageSync("courseByWeeks"));
+
+onLoad(() => {
+  if (briefList.value.length === 0) {
+    refreshClassTable();
+    briefList.value = formatClassTable();
+  }
+});
 </script>
 
 <style></style>
---
+++ src/utils/formatClassTable.ts
@@ -0,0 +1,68 @@
+function expandRanges(ranges: string[]): number[] {
+  const result: number[] = [];
+
+  for (const range of ranges) {
+    const [rangeStr, parity] = range.split("(");
+    const [startStr, endStr] = rangeStr.split("-");
+    if (!endStr) {
+      result.push(parseInt(rangeStr));
+      continue;
+    }
+    const start = parseInt(startStr);
+    const end = parseInt(endStr);
+
+    for (let i = start; i <= end; i++) {
+      if (
+        !parity ||
+        (parity === "单)" && i % 2 === 1) ||
+        (parity === "双)" && i % 2 === 0)
+      ) {
+        result.push(i);
+      }
+    }
+  }
+  return result;
+}
+
+const formatClassTable = () => {
+  const briefList: any = [];
+
+  let classTable = uni.getStorageSync("classTable");
+
+  classTable.value = uni.getStorageSync("classTable");
+  classTable.value = classTable.value.kbList;
+  const neededList = [
+    "kcmc",
+    "cdmc",
+    "kclb",
+    "kcxz",
+    "xm",
+    "xf",
+    "jc",
+    "xqj",
+    "zcd",
+  ];
+  let courseByWeeks: any = {};
+  classTable.value.forEach((item: any) => {
+    const brief: any = [];
+
+    neededList.forEach((key: string) => {
+      if (key === "zcd") {
+        item[key] = item[key].replace(/周/g, "").split(",");
+        item[key] = expandRanges(item[key]);
+        for (let i = 0; i < item[key].length; i++) {
+          if (!courseByWeeks[item[key][i]]) {
+            courseByWeeks[item[key][i]] = [];
+          }
+          courseByWeeks[item[key][i]].push(brief);
+        }
+      }
+      brief.push(item[key]);
+    });
+    briefList.push(brief);
+  });
+  uni.setStorageSync("courseByWeeks", courseByWeeks);
+  return courseByWeeks;
+};
+
+export default formatClassTable;
